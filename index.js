c1e293d3c44d18e8e6f6e6d0901e895be7f03ae0
Yeison <enmanuelquiterios@gmail.com>
2022-02-04 23:53:42 -0500

    Connecting MongoDB4

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParse: true }, () => {
+  console.log("Connected to MongoDB");
+});
+
 app.listen(8800, () => {
   console.log("Backend server Running in PORT 8080");
 });
